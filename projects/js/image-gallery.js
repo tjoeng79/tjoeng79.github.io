0b71dde20bfd339d2571bd44b55d32a19f2821e5
tjoeng79 <nenfie.tjoeng@gmail.com>
2026-05-05 15:12:59 +0700

    Update image-gallery project

--- projects/js/image-gallery.js
+++ projects/js/image-gallery.js
@@ -18,3 +18,49 @@ thumbsArea_div.addEventListener('click', function(e){
     e.target.classList.add('active');
   }
 });
+
+let imgIndex = 0;
+let slideDirection = 1;
+
+setInterval(() => {
+  if (slideDirection == 1) {
+    slideNext();
+  } else {
+    slidePrev();
+  }
+
+}, 3000);
+
+const nextBtn = document.getElementById('next-btn');
+nextBtn.onclick = function() {
+  slideDirection = 1;
+  slideNext();
+};
+
+const prevBtn = document.getElementById('prev-btn');
+prevBtn.onclick = function() {
+  slideDirection = 2;
+  slidePrev();
+};
+
+function slideNext() {
+  if (imgIndex == thumbs_img.length - 1) {
+    imgIndex = 0;
+  } else {
+    imgIndex++;
+  }
+  showSlide();
+}
+
+function slidePrev() {
+  if (imgIndex > 0) {
+    imgIndex--;
+  } else {
+    imgIndex = thumbs_img.length - 1;
+  }
+  showSlide();
+}
+
+function showSlide() {
+  mainImage_img.src = thumbs_img[imgIndex].src;
+}
